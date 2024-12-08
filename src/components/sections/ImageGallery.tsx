09ebc3f9d6304ea0c5cf467dc895accf990ee45b
yongveloper <dev_yong@kakao.com>
2024-12-08 18:49:16 +0900

    image swiper

--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -2,20 +2,48 @@ import classNames from 'classnames/bind';
 
 import Section from '@shared/Section';
 import styles from './ImageGallery.module.scss';
+import ImageViewer from '../ImageViewer';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function ImageGallery({ images }: { images: string[] }) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const open = selectedIndex > -1;
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIndex(idx);
+  };
+
+  const handleClose = () => {
+    setSelectedIndex(-1);
+  };
+
   return (
-    <Section title="사진첩">
-      <ul className={cx('wrap-images')}>
-        {images.map((src, index) => (
-          <li key={index} className={cx('wrap-image')}>
-            <img src={src} alt={`사진첩 이미지 ${index + 1}`} />
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Section title="사진첩">
+        <ul className={cx('wrap-images')}>
+          {images.map((src, index) => (
+            <li
+              key={index}
+              className={cx('wrap-image')}
+              onClick={() => {
+                handleSelectedImage(index);
+              }}
+            >
+              <img src={src} alt={`사진첩 이미지 ${index + 1}`} />
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIndex={selectedIndex}
+        onClose={handleClose}
+      />
+    </>
   );
 }
 
